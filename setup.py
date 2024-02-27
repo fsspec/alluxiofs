f0a952518374a4de82a74936ce7d545cedd14585
Siyuan Sheng <ssyssy@umich.edu>
2024-02-27 15:22:43 -0800

    New version 1.0.0

--- setup.py
+++ setup.py
@@ -3,12 +3,15 @@ from setuptools import setup
 
 setup(
     name="alluxiofs",
-    version="0.1",
+    version="1.0.0",
+    description="Alluxio Fsspec 1.0.0 provides Alluxio filesystem spec implementation.",
+    url="https://github.com/fsspec/alluxiofs",
     packages=find_packages(exclude=["tests", "tests.*"]),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         "fsspec",
+        "alluxio==1.0.0",
     ],
     extras_require={"tests": ["pytest"]},
     python_requires=">=3.8",
