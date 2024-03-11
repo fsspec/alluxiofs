a56395da338815077f08361c89842bc0f5ee5a77
Lu Qiu <luqiujob@gmail.com>
2024-03-11 12:40:58 -0700

    Skip S3 fallback tests

--- alluxiofs/tests/derived/s3/s3_fallback_test.py
+++ alluxiofs/tests/derived/s3/s3_fallback_test.py
@@ -1,15 +1,19 @@
 import fsspec.tests.abstract as abstract
+import pytest
 
 from alluxiofs.tests.derived.s3.s3_fallback_fixtures import S3FallbackFixtures
 
 
+@pytest.mark.skip(reason="S3 credentials not set.")
 class TestAlluxioGet(abstract.AbstractGetTests, S3FallbackFixtures):
     pass
 
 
+@pytest.mark.skip(reason="S3 credentials not set.")
 class TestAlluxioCopy(abstract.AbstractCopyTests, S3FallbackFixtures):
     pass
 
 
+@pytest.mark.skip(reason="S3 credentials not set.")
 class TestAlluxioPut(abstract.AbstractPutTests, S3FallbackFixtures):
     pass
