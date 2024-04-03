d864fed29593069720f762e111fe113d396c6ae4
Lucy Ge <lucy.ge@alluxio.com>
2024-04-03 15:21:17 -0700

    add REST getfilelist getfileinfo tests

--- tests/benchmark/AlluxioRESTBench.py
+++ tests/benchmark/AlluxioRESTBench.py
@@ -1,3 +1,4 @@
+import json
 import random
 import re
 from enum import Enum
@@ -6,7 +7,7 @@ import requests
 from requests.adapters import HTTPAdapter
 
 from alluxiofs.client.const import (
-    ALLUXIO_WORKER_HTTP_SERVER_PORT_DEFAULT_VALUE,
+    ALLUXIO_WORKER_HTTP_SERVER_PORT_DEFAULT_VALUE, LIST_URL_FORMAT, GET_FILE_STATUS_URL_FORMAT,
 )
 from alluxiofs.client.const import FULL_PAGE_URL_FORMAT
 from tests.benchmark.AbstractBench import AbstractArgumentParser
@@ -43,6 +44,13 @@ class AlluxioRESTArgumentParser(AbstractArgumentParser):
             required=False,
             help="page id start and end range, <str_pageid>-<end_pageid> (e.g. 0-39, end inclusive)",
         )
+        # ListFiles/GetFileInfo args
+        self.parser.add_argument(
+            "--path",
+            type=str,
+            required=False,
+            help="path to do ListFiles or GetFileInfo, e.g. s3://bucket1/dir1",
+        )
 
     def parse_args(self, args=None, namespace=None):
         args = self.parser.parse_args(args, namespace)
@@ -55,15 +63,15 @@ class AlluxioRESTBench(AbstractBench):
 
     def init(self):
         self.validate_args()
-        self.worker = self.args.worker_hosts.split(",")[0]
+        self.worker_host = self.args.worker_hosts.split(",")[0]
         self.page_id_range = None
+        self.path = self.args.path
         if self.args.page_id_range is not None:
             match = re.match(r"\d+-\d+", self.args.page_id_range)
             if match:
                 nums = [int(x) for x in match.group().split("-")]
                 self.page_id_range = (nums[0], nums[1])
                 # print(f"{self.page_id_range}")
-
         # Init session
         self.session = requests.Session()
         adapter = HTTPAdapter(pool_connections=1, pool_maxsize=1)
@@ -71,12 +79,11 @@ class AlluxioRESTBench(AbstractBench):
 
     def execute(self):
         if self.args.op == Op.GetPage.name:
-            # print(f"Executing AlluxioRESTBench! Op:{self.args.op}")
             self.testGetPage()
-        elif self.args.op == Op.GetPage.name:
-            pass
+        elif self.args.op == Op.GetFileInfo.name:
+            self.testGetFileInfo()
         elif self.args.op == Op.ListFiles.name:
-            pass
+            self.testListFiles()
         elif self.args.op == Op.PutPage.name:
             pass
         else:
@@ -98,7 +105,6 @@ class AlluxioRESTBench(AbstractBench):
                 )
 
             if self.args.page_id_range is not None:
-                # print(f"self.args.page_id_range:{self.args.page_id_range}")
                 match = re.match(r"\d+-\d+", self.args.page_id_range)
                 if match:
                     nums = [int(x) for x in match.group().split("-")]
@@ -106,11 +112,15 @@ class AlluxioRESTBench(AbstractBench):
                         raise Exception("Invalid page_id_range")
                 else:
                     raise Exception("Incorrect page_id_range param passed.")
-        elif self.args.op == Op.ListFiles.name:
-            pass
+        elif self.args.op == Op.ListFiles.name \
+            or self.args.op == Op.GetFileInfo.name:
+            required_args = [self.args.path]
+            required_args_absence = any(arg is None for arg in required_args)
+            if required_args_absence:
+                raise Exception(
+                    f"Missing args for {self.args.op}, required args:[path]"
+                )
         elif self.args.op == Op.PutPage.name:
-            pass
-        elif self.args.op == Op.GetFileInfo.name:
             pass
 
     def testGetPage(self):
@@ -128,4 +138,44 @@ class AlluxioRESTBench(AbstractBench):
             len(response.content)
             content_len = len(response.content)
         except Exception as e:
-            print("Exception during testGetPage:", e)
+            raise Exception(
+                f"Error ListFiles, path:{self.path}: error {e}"
+            ) from e
+
+    def testListFiles(self):
+        params = {"path": self.path}
+        try:
+            response = self.session.get(
+                LIST_URL_FORMAT.format(
+                    worker_host= self.worker_host,
+                    http_port=ALLUXIO_WORKER_HTTP_SERVER_PORT_DEFAULT_VALUE
+                ),
+                params=params,
+            )
+            response.raise_for_status()
+            # just read full content but do nothing
+            result = json.loads(response.content)
+        except Exception as e:
+            raise Exception(
+                f"Error ListFiles, path:{self.path}: error {e}"
+            ) from e
+
+    def testGetFileInfo(self):
+        params = {"path": self.path}
+        try:
+            response = self.session.get(
+                GET_FILE_STATUS_URL_FORMAT.format(
+                    worker_host=self.worker_host,
+                    http_port=ALLUXIO_WORKER_HTTP_SERVER_PORT_DEFAULT_VALUE,
+                ),
+                params=params,
+            )
+            response.raise_for_status()
+            data = json.loads(response.content)[0]
+        except Exception as e:
+            raise Exception(
+                f"Error GetFileInfo, path:{self.path}: error {e}"
+            ) from e
+
+    def testPutPage(self):
+        pass
