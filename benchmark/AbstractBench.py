8c377b34a54c705b46e695082e258ff62f8f2314
lucyge2022 <111789461+lucyge2022@users.noreply.github.com>
2024-05-09 14:28:39 -0700

    Add Ray data bench and reduce rust realm data copy (#42)
    add ray_single_node_image_loader_microbenchmark.py from https://gist.github.com/stephanie-wang/88500403e701537568383ef2e181768c in blog https://www.anyscale.com/blog/fast-flexible-scalable-data-loading-for-ml-training-with-ray-data
    reduce data copies in rust make multi http calls

--- benchmark/AbstractBench.py
+++ benchmark/AbstractBench.py
@@ -79,10 +79,14 @@ class AbstractAlluxioFSSpecTraverseBench(AbstractBench, ABC):
 
     def init(self):
         # protocol = self.get_protocol(self.args.path)
-        alluxio_options = {
-            # "alluxio.common.extension.enable": "True",
-            "alluxio.worker.page.store.page.size": "1MB"
-        }
+        alluxio_options = {}
+        if self.args.use_alluxiocommon:
+            alluxio_options["alluxio.common.extension.enable"] = "True"
+        if self.args.page_size:
+            alluxio_options[
+                "alluxio.worker.page.store.page.size"
+            ] = self.args.page_size
+        print(f"options for AlluxioFileSystem:{alluxio_options}")
         self.alluxio_fs = AlluxioFileSystem(
             etcd_hosts=self.args.etcd_hosts,
             worker_hosts=self.args.worker_hosts,
