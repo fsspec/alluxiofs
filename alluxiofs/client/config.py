c6e89e4dba5b852bf411ea9764e7f7aa0c61a28b
Sibyl Yang <yangtingyusibyl@gmail.com>
2024-06-27 12:07:38 -0700

    change to assert

--- alluxiofs/client/config.py
+++ alluxiofs/client/config.py
@@ -41,51 +41,49 @@ class AlluxioClientConfig:
             etcd_refresh_workers_interval (int, optional): The interval to refresh worker list from ETCD membership service periodically.
                 All negative values mean the service is disabled.
         """
-        if not (etcd_hosts or worker_hosts):
-            raise ValueError(
-                "Must supply either 'etcd_hosts' or 'worker_hosts'"
-            )
-        if etcd_hosts and worker_hosts:
-            raise ValueError(
-                "Supply either 'etcd_hosts' or 'worker_hosts', not both"
-            )
-        if not isinstance(etcd_port, int) or not (1 <= etcd_port <= 65535):
-            raise ValueError(
-                "'etcd_port' should be an integer in the range 1-65535"
-            )
-        if not isinstance(worker_http_port, int) or not (
+
+        assert (
+            etcd_hosts or worker_hosts
+        ), "Must supply either 'etcd_hosts' or 'worker_hosts'"
+
+        assert not (
+            etcd_hosts and worker_hosts
+        ), "Supply either 'etcd_hosts' or 'worker_hosts', not both"
+
+        assert isinstance(etcd_port, int) and (
+            1 <= etcd_port <= 65535
+        ), "'etcd_port' should be an integer in the range 1-65535"
+
+        assert isinstance(worker_http_port, int) and (
             1 <= worker_http_port <= 65535
-        ):
-            raise ValueError(
-                "'worker_http_port' should be an integer in the range 1-65535"
-            )
-        if not isinstance(concurrency, int) or concurrency <= 0:
-            raise ValueError("'concurrency' should be a positive integer")
-        if not isinstance(etcd_refresh_workers_interval, int):
-            raise ValueError(
-                "'etcd_refresh_workers_interval' should be an integer"
-            )
+        ), "'worker_http_port' should be an integer in the range 1-65535"
+
+        assert (
+            isinstance(concurrency, int) and concurrency > 0
+        ), "'concurrency' should be a positive integer"
+
+        assert isinstance(
+            etcd_refresh_workers_interval, int
+        ), "'etcd_refresh_workers_interval' should be an integer"
+
         self.etcd_hosts = etcd_hosts
         self.worker_hosts = worker_hosts
         self.etcd_port = etcd_port
         self.worker_http_port = worker_http_port
         self.etcd_refresh_workers_interval = etcd_refresh_workers_interval
-        if (
-            not isinstance(hash_node_per_worker, int)
-            or hash_node_per_worker <= 0
-        ):
-            raise ValueError(
-                "'hash_node_per_worker' should be a positive integer"
-            )
+
+        assert (
+            isinstance(hash_node_per_worker, int) and hash_node_per_worker > 0
+        ), "'hash_node_per_worker' should be a positive integer"
 
         self.hash_node_per_worker = hash_node_per_worker
         self.page_size = humanfriendly.parse_size(page_size, binary=True)
         self.cluster_name = cluster_name
 
-        if (etcd_username is None) != (etcd_password is None):
-            raise ValueError(
-                "Both ETCD username and password must be set or both should be unset."
-            )
+        assert (etcd_username is None) == (
+            etcd_password is None
+        ), "Both ETCD username and password must be set or both should be unset."
+
         self.etcd_username = etcd_username
         self.etcd_password = etcd_password
         self.concurrency = concurrency
