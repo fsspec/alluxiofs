d0baab1a5d6932041335b073f0fb2d22ccfcf578
littleEast7 <xiaodong163831@gmail.com>
2025-03-05 11:17:36 +0800

    delete getpage

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="alluxiofs",
-    version="1.0.6",
+    version="1.0.7",
     description="Alluxio Fsspec provides Alluxio filesystem spec implementation.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
