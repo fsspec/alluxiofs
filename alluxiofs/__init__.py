461d79e350a108ff6fb1e06e0bfd3c65bfffc79c
littleEast7 <xiaodong163831@gmail.com>
2025-11-05 22:18:43 +0800

    add client-level memory cache

--- alluxiofs/__init__.py
+++ alluxiofs/__init__.py
@@ -1,5 +1,5 @@
-from alluxiofs.client import AlluxioClient
-from alluxiofs.core import AlluxioFileSystem
-from alluxiofs.core import setup_logger
+from .client import AlluxioClient
+from .core import AlluxioFileSystem
+from .core import setup_logger
 
 __all__ = ["AlluxioFileSystem", "AlluxioClient", "setup_logger"]
